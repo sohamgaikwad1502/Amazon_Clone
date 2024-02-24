4a714157b8f0f535bf2f9ed0f040f4c19109e251
soham <sohamkgaikwad@gmail.com>
2024-02-24 16:44:17 +0530

    moved add to cart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,2 +1,25 @@
 export const cart = [];
 
+export function addToCart(productId){
+    let matchingItem ;
+    cart.forEach((cartItem) => {
+      if(cartItem.productId === productId){
+        matchingItem = cartItem;
+      }
+    });
+  
+    if(matchingItem){
+      matchingItem.quantity += 1 ;
+    }
+    else 
+    {
+      cart.push(
+        {
+          productId : productId,
+          quantity : 1 
+  
+        }
+      );
+  
+    }
+  }
--- js_files/amazon.js
+++ js_files/amazon.js
@@ -1,5 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart,addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
+
 
 let product_html_final ="" ;
 products.forEach((product)=> {
@@ -57,40 +58,24 @@ products.forEach((product)=> {
 
 document.querySelector('.products-grid').innerHTML = product_html_final;
 
+
+
+function cartQuantityUpdate()
+{
+  let cartQuantity =  0 ; 
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click',() => {
       const productId = button.dataset.productId;
-      
-
-      let matchingItem ;
-      cart.forEach((item) => {
-        if(item.productId === productId){
-          matchingItem = item;
-        }
-      });
-
-      if(matchingItem){
-        matchingItem.quantity += 1 ;
-      }
-      else 
-      {
-        cart.push(
-          {
-            productId : productId,
-            quantity : 1 
-  
-          }
-        );
-
-      }
-
-    let cartQuantity =  0 ; 
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    
-    });
+      addToCart(productId);
+      cartQuantityUpdate();
+    });   
   });
 
